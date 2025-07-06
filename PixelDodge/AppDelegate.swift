180ce8833a99f0ffd1404ffc0d1e2919aec1d346
kirsch garrix <kirschgarrix@google.com>
2025-07-06 23:30:31 +0800

    Initial Commit

---
+++ PixelDodge/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  PixelDodge
+//
+//  Created by Kirsch Garrix on 2025/7/6.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
---
+++ PixelDodgeTests/PixelDodgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  PixelDodgeTests.swift
+//  PixelDodgeTests
+//
+//  Created by Kirsch Garrix on 2025/7/6.
+//
+
+import Testing
+@testable import PixelDodge
+
+struct PixelDodgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
